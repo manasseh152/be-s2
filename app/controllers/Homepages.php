95dc3e0d70ca61e4a1c952697dac091f0b1473fd
manasseh152 <manasseh2424@gmail.com>
2023-01-29 23:19:03 +0100

    Bug Fix

--- app/controllers/Homepages.php
+++ app/controllers/Homepages.php
@@ -8,19 +8,10 @@ use App\Libraries\Controller;
 
 class HomePages extends Controller
 {
-  private $userModel;
-
-  public function __construct()
-  {
-    $this->userModel = $this->model('User');
-  }
-
   public function index()
   {
-    $users = $this->userModel->getUsers();
     $data = [
       'title' => "Home",
-      'users' => $users
     ];
     // $this->view('homepages/index', $data);
     $this->view('homepages/index', $data);
